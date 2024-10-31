9f2838739687174894df05d3765fa28522c4f39b
delldevmann <144141627+delldevmann@users.noreply.github.com>
2024-10-31 17:55:11 -0400

    Update newday.py

--- project/newday.py
+++ project/newday.py
@@ -1,28 +1,33 @@
 import os
-from main3 import start
-from main5 import start_new
 import nltk
 from pathlib import Path
+from main3 import start
+from main5 import start_new
+
+# Download necessary NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
-csv_folder = Path(__file__).resolve().parent  # Get the directory of the script
+
+# Get the directory of the script
+csv_folder = Path(__file__).resolve().parent  
+
+# Define file paths for various CSVs
 files = [
     csv_folder/'india.csv',
     csv_folder/'world.csv',
     csv_folder/'business.csv',
     csv_folder/'tech.csv',
     csv_folder/'sports.csv'
-         ]
+]
 
+# Truncate files if they exist
 for filepath in files:
-    # Check if the file exists
     if os.path.exists(filepath):
-        # Open the file in binary write mode
         with open(filepath, "wb") as f:
-            # Truncate the file
             f.truncate()
         print(f"File '{filepath}' truncated successfully.")
     else:
         print(f"File '{filepath}' does not exist.")
 
+# Run the required startup functions
 start_new()
